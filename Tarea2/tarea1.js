6142f033eee81731fa421c91bff5056b9b5d8814
roque76 <jhonjaimemadrid02@gmail.com>
2024-02-14 22:07:13 -0500

    Change in format

---
+++ Tarea2/tarea1.js
@@ -0,0 +1,6 @@
+/// Ejercisio 1
+function Ejercisio1 (){
+    number1 = Number(prompt("(Ejercisio 1) Ingresar primer numero: "))
+    number2 = Number(prompt("Ingresar segundo numero: "))
+    alert(`El resultad`)
+}
